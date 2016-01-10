596e7efd497c31229ca2a9e71e40831fdc17d059
Saurabh <saurabhhindlekar@hotmail.com>
2016-01-10 12:25:37 -0500

    update2

--- update2/README.txt
+++ update2/README.txt
@@ -7,4 +7,12 @@ The Hip joint forces are less and not that visible. Should they be made more vis
 The knee joint is what is is driving the simulation according to me. 
 The angle of the knee joint is also too large I guess, the pit has to be upright. Currently its not. 
 The puppetier mechanism hasn't been achived yet. 
-The change of state on collision should be gradual. 
+
+Update 2 (Scene to be opened - bouncymovingmech)
+
+All three joints activated. 
+The Mech is still bouncy, the angles and forces have to tuned so that its not. 
+The version of Unity used is 5.1.2
+I think it will work with 5.2 too. But breaks down in 5.3 
+
+
---
+++ update2/icanseeformiles/Assets/scripts/rBaseScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rBaseScript : MonoBehaviour {
+    public Rigidbody rb;
+    static public bool hitGround = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.constraints = RigidbodyConstraints.FreezePositionX;
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Plane")
+        {
+            hitGround = true;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.name == "Plane")
+        {
+            hitGround = false;
+        }
+    }
+}
